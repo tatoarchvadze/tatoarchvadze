259c01c8c478a687e7bf65fe51fa24b1d0f06d2a
tatoarchvadze <164230015+tatoarchvadze@users.noreply.github.com>
2024-04-03 16:54:07 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+places_to_visit = ['NYC', 'japan','austria','antarctida']
+
+print(sorted(places_to_visit))
+print(places_to_visit)
+print(sorted(places_to_visit, reverse=True))
+
+
+places_to_visit.sort()
+
+
+print(places_to_visit)
+
+print('hi')
+
+print(' ,'. join(places_to_visit))
